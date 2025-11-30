4bd6aa789cfce1a3892bf0332c8a14f907e25100
Ibukun Patunola <patunolaibukundavid001@gmail.com>
2025-11-30 08:54:59 +0000

    added more nav component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,43 +1,53 @@
 import * as Icons from "lucide-react";
 
 const navLinks = [
-    {
-        name: "Home",
-        icon: "Home"
-    },
-    {
-        name: "Setting",
-        icon: "Settings"
-    }
-]
+  {
+    name: "Home",
+    icon: "Home",
+  },
+	{
+		name: "Map",
+		icon: "Map"
+	},
+  {
+    name: "Setting",
+    icon: "Settings",
+  },
+	 {
+    name: "Transfer",
+    icon: "refresh-ccw",
+  },
+	{
+		name: "Profile",
+		icon: "user"
+	}
+];
 
-function Navigation () {
-    return (
-        <div className="flex gap-4 text-red-500">
-        {
-            navLinks.map(link => {
-                const Icon = Icons[link.icon];
-                
-                return (
-                    <Nav 
-                        icon={Icon}
-                        name={link.name}
-                        key={link.name}
-                    />
-                )
-            })
-        }
-        </div>
-    )
+function Navigation() {
+  return (
+    <div className="flex gap-4 text-red-500">
+      {navLinks.map((link) => {
+        const Icon = Icons[link.icon];
+
+        return (
+          <Nav
+            icon={Icon}
+            name={link.name}
+            key={link.name}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-function Nav ({ icon: Icon, name }) {
-    return (
-        <div>
-            <Icon size={24} />
-            <span>{name}</span>
-        </div>
-    )
+function Nav({ icon: Icon, name }) {
+  return (
+    <div className="grid gap-5 bg-grey-300">
+      <Icon size={24} />
+      <span>{name}</span>
+    </div>
+  );
 }
 
-export default Navigation
+export default Navigation;
