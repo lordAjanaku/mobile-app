e3abdaa6d8f8f292a27fa3d54de5cf76c20a4f92
Ibukun Patunola <patunolaibukundavid001@gmail.com>
2025-11-30 10:32:51 +0000

    add logic to the Navigation component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,39 +1,30 @@
+import { useState } from 'react';
 import * as Icons from "lucide-react";
 
 const navLinks = [
-  {
-    name: "Home",
-    icon: "Home",
-  },
-  {
-    name: "Map",
-    icon: "Map"
-  },
-  {
-    name: "Setting",
-    icon: "Settings",
-  },
-  {
-    name: "Transfer",
-    icon: "RefreshCcw",
-  },
-  {
-    name: "Profile",
-    icon: "User"
-  }
+  { name: "Home", icon: "Home", active: true },
+  { name: "Map", icon: "Map", active: false },
+  { name: "Setting", icon: "Settings", active: false },
+  { name: "Transfer", icon: "RefreshCcw", active: false },
+  { name: "Profile", icon: "User", active: false }
 ];
 
 function Navigation() {
+  const [navs, setNavs] = useState(navLinks);
+
   return (
-    <div className="flex gap-4 text-red-500">
-      {navLinks.map((link) => {
+    <div className="flex gap-1 text-gray-300 outline">
+      {navs.map((link) => {
         const Icon = Icons[link.icon];
 
         return (
           <Nav
+            key={link.name}
+            setNavs={setNavs}
+            navs={navs}
             icon={Icon}
             name={link.name}
-            key={link.name}
+            active={link.active}
           />
         );
       })}
@@ -41,10 +32,27 @@ function Navigation() {
   );
 }
 
-function Nav({ icon: Icon, name }) {
+function Nav({ icon: Icon, name, active, navs, setNavs }) {
+
+  function handleNavChange(name) {
+    const updated = navs.map(nav =>
+      nav.name === name
+        ? { ...nav, active: true }
+        : { ...nav, active: false }
+    );
+
+    setNavs(updated);
+  }
+
   return (
-    <div className="grid gap-5 bg-gray-300">
-      <Icon size={24} />
+    <div className="grid gap-5 outline outline-red-50">
+      <Icon
+        size={24}
+        strokeWidth={active ? 0 : 1}
+        absoluteStrokeWidth={true}
+        fill={active ? "currentColor" : "none"}
+        onClick={() => handleNavChange(name)}
+      />
       <span>{name}</span>
     </div>
   );
